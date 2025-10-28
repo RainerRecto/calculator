1b16026bed034bb1514fa4c7250d6686847afe7a
RainerRecto <c22-1826-466@uphsl.edu.ph>
2025-10-28 13:36:18 +0800

    Multiplication commit

--- calculator.js
+++ calculator.js
@@ -1,12 +1,16 @@
 let x = 1
 let y = 2
 
-console.log ("ADD", add(x, y))
-console.log ("SUBTRACT", subtract(x, y))
+console.log("ADD", add(x, y))
+console.log("SUBTRACT", subtract(x, y))
+console.log("MULTIPLY", multiply(x, y))
 
 function add(x,y){
   return x+y
 }
 function subtract(x,y){
   return x-y
+}
+function multiply(x,y){
+  return x*y
 }
