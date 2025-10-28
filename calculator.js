80a84f0235d90d5bf8c21a13f7a3298b0db0db8c
RainerRecto <c22-1826-466@uphsl.edu.ph>
2025-10-28 13:45:45 +0800

    Remove Add Commit

--- calculator.js
+++ calculator.js
@@ -1,14 +1,13 @@
 let x = 1
 let y = 2
 
-console.log("ADD", add(x, y))
+
 console.log("SUBTRACT", subtract(x, y))
 console.log("MULTIPLY", multiply(x, y))
 console.log("DIVIDE", divide(x,y))
 console.log("SQUARE", square(x))
 
-function add(x,y){
-  return x+y
+
 }
 function subtract(x,y){
   return x-y
