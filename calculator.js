4184fa2d8d10543d828de0e181f89fb30f833033
RainerRecto <c22-1826-466@uphsl.edu.ph>
2025-10-28 13:38:16 +0800

    Division commit

--- calculator.js
+++ calculator.js
@@ -4,6 +4,7 @@ let y = 2
 console.log("ADD", add(x, y))
 console.log("SUBTRACT", subtract(x, y))
 console.log("MULTIPLY", multiply(x, y))
+console.log("DIVIDE", divide(x,y))
 
 function add(x,y){
   return x+y
@@ -14,3 +15,7 @@ function subtract(x,y){
 function multiply(x,y){
   return x*y
 }
+function divide(x,y){
+  return x/y
+}
+
